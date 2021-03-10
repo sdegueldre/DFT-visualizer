ca2e5a151b43e7508f34999418acef1cc0a08b9e
Samuel Degueldre <sad@odoo.com>
2021-03-10 20:09:47 +0100

    [IMP/REF] Improve and fix a lot of things:
    - Add an eslint config
    - Fix code to conform to the new config
    - Position all elements in overlay as single block so they're well
    aligned on all browsers
    - Add FPS counter
    - Restore ability to show the spectrum of the default file
    - Make user files play in the audio editor and synchronize visuals with
    that, allowing them to control the audio normally
    - Removed some dead code
    - Add a (probably useless) per-frame cache for FFT

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
-import "babel-polyfill";
-
-const ft = require('fourier-transform/asm');
-const db = require('decibels');
-const soundfile = require('../assets/audio/Komiku - Mushrooms.mp3');
-const audioElt = document.createElement('audio');
-audioElt.src = soundfile;
-audioElt.controls = true;
-audioElt.loop = true;
-document.body.appendChild(audioElt);
+import ft from 'fourier-transform/asm';
+const audioElt = document.querySelector('audio');
+const soundfile = audioElt.src;
 
 const cv = document.querySelector('canvas');
 cv.width = window.innerWidth;
@@ -19,113 +12,144 @@ gradient.addColorStop(0, '#30bf33');
 gradient.addColorStop(1, '#008ce2');
 ctx.fillStyle = gradient;
 
+// TODO: this seems shitty. Should probably get sample rate from file...
 const samplingRate = 44100;
-const audioElem = document.querySelector('audio');
-let leftSamples;
-let rightSamples;
+let leftSamples, rightSamples;
+let cache = {};
+
+const audioCtx = new AudioContext();
+
+function loadAudio(fileOrBlob) {
+  const reader = new FileReader();
+  const objectUrl = URL.createObjectURL(fileOrBlob);
+  audioElt.src = objectUrl;
+  reader.readAsArrayBuffer(fileOrBlob);
+  reader.onload = async () => {
+    const audioBuffer = await audioCtx.decodeAudioData(reader.result);
+    leftSamples = audioBuffer.getChannelData(0);
+    rightSamples = audioBuffer.getChannelData(1);
+    if (!playing)
+      playAnim();
+    console.log('Samples acquired');
+  }
+}
+
+// Load base audio file.
+(async () => {
+  const response = await fetch(soundfile);
+  loadAudio(await response.blob());
+})();
 
 const input = document.querySelector('input');
-let startTime = 0;
-let audioCtx;
-let bufferSource;
+input.addEventListener('change', async () => {
+  cache = {};
+  loadAudio(input.files[0]);
+});
 
-input.addEventListener('change', async (e) => {
-    const reader = new FileReader();
-    reader.readAsArrayBuffer(input.files[0]);
-    audioCtx = new AudioContext();
-    reader.onload = async () => {
-        if (bufferSource) {
-            console.log('There was already a buffer source, stopping it...', bufferSource);
-            bufferSource.stop();
-        }
-        const audioBuffer = await audioCtx.decodeAudioData(reader.result);
-        bufferSource = audioCtx.createBufferSource();
-        bufferSource.buffer = audioBuffer;
-        bufferSource.connect(audioCtx.destination);
-        bufferSource.start();
-        console.log(bufferSource);
-        console.log(audioBuffer);
-        leftSamples = audioBuffer.getChannelData(0);
-        rightSamples = audioBuffer.getChannelData(1);
-        startTime = audioCtx.currentTime;
-        if (!playing)
-            playAnim();
-        console.log('Samples acquired');
-    }
+window.addEventListener('resize', () => {
+  cv.width = window.innerWidth;
+  cv.height = window.innerHeight;
+  ctx = cv.getContext('2d');
 
-    window.addEventListener('resize', () => {
-        cv.width = window.innerWidth;
-        cv.height = window.innerHeight;
-        ctx = cv.getContext('2d');
+  gradient = ctx.createLinearGradient(0, 0, cv.width, cv.height);
+  gradient.addColorStop(0, '#30bf33');
+  gradient.addColorStop(1, '#008ce2');
+  ctx.fillStyle = gradient;
 
-        gradient = ctx.createLinearGradient(0, 0, cv.width, cv.height);
-        gradient.addColorStop(0, '#30bf33');
-        gradient.addColorStop(1, '#008ce2');
-        ctx.fillStyle = gradient;
-
-        if (!playing)
-            playAnim();
-    });
+  if (!playing)
+    playAnim();
 });
 
 const halfWinsize = 4096;
 
 function getFrame(time) {
-    let left = leftSamples.slice(Math.max(0, ~~(time * samplingRate) - halfWinsize), Math.max(2 * halfWinsize, ~~(time * samplingRate) + halfWinsize));
-    let right = rightSamples.slice(Math.max(0, ~~(time * samplingRate) - halfWinsize), Math.max(2 * halfWinsize, ~~(time * samplingRate) + halfWinsize));
+  const centerSample = roundTo(time * samplingRate, samplingRate / 30);
+  // TODO: make cache actually useful
+  if (!cache[centerSample]) {
+    let left = leftSamples.slice(Math.max(0, centerSample - halfWinsize), Math.max(2 * halfWinsize, centerSample + halfWinsize));
+    let right = rightSamples.slice(Math.max(0, centerSample - halfWinsize), Math.max(2 * halfWinsize, centerSample + halfWinsize));
     left = left.map((v, i) => v * (Math.cos(i / left.length * 2 * Math.PI + Math.PI) + 1));
     right = right.map((v, i) => v * (Math.cos(i / right.length * 2 * Math.PI + Math.PI) + 1));
-    if (left.length != 2 * halfWinsize)
-        return [
-            [],
-            []
-        ];
-    return [
+    if (left.length != 2 * halfWinsize) {
+      cache[centerSample] = [
+        [],
+        [],
+      ];
+    } else {
+      cache[centerSample] = [
         [...ft(left)],
-        [...ft(right)]
-    ];
+        [...ft(right)],
+      ];
+    }
+  }
+  return cache[centerSample];
 }
 
 const ln = Math.log;
 const hScale = 1 / ln(halfWinsize + 1); // horizontal scaling factor
 
 let playing = false;
+let lastFrameTime;
+let frameTimes = [];
 
 function playAnim() {
-    playing = true;
-    ctx.clearRect(0, 0, cv.width, cv.height);
-    const currentTime = audioCtx.currentTime - startTime;
-    const data = getFrame(currentTime);
+  playing = true;
+  ctx.clearRect(0, 0, cv.width, cv.height);
+  const data = leftSamples ? getFrame(audioElt.currentTime) : [];
 
-    const frame = data.map((channel, i) => channel.map((v, j) => {
-        const x = ln(j + 1) * cv.width * hScale;
-        return ({
-            x: x,
-            y: ((i % 2) ? v : -v) * cv.height / 2 * (Math.pow(25, x / cv.width) / 2) + cv.height / 2
-        })
-    }));
-
-    frame.forEach(channel => {
-        if (channel.length == 0)
-            return;
-        ctx.beginPath();
-        ctx.moveTo(0, cv.height / 2);
-        ctx.lineTo(0, channel[0].y);
-
-        for (let i = 1; i < channel.length - 1; i++) {
-            const point = channel[i];
-            const next = channel[i + 1];
-            const control = {
-                x: (point.x + next.x) * 0.5,
-                y: (point.y + next.y) * 0.5
-            };
-            ctx.quadraticCurveTo(point.x, point.y, control.x, control.y);
-        }
-
-        ctx.lineTo(cv.width, channel[channel.length - 1].y);
-        ctx.lineTo(cv.width, cv.height / 2);
-        ctx.fill();
+  const channels = data.map((channel, i) => channel.map((v, j) => {
+    const x = ln(j + 1) * cv.width * hScale;
+    return ({
+      x: x,
+      y: ((i % 2) ? v : -v) * cv.height / 2 * (Math.pow(25, x / cv.width) / 2) + cv.height / 2,
     })
+  }));
 
-    window.requestAnimationFrame(playAnim);
+  channels.forEach(channel => {
+    if (channel.length == 0)
+      return;
+    ctx.beginPath();
+    ctx.moveTo(0, cv.height / 2);
+    ctx.lineTo(0, channel[0].y);
+
+    for (let i = 1; i < channel.length - 1; i++) {
+      const point = channel[i];
+      const next = channel[i + 1];
+      const control = {
+        x: (point.x + next.x) * 0.5,
+        y: (point.y + next.y) * 0.5,
+      };
+      // Curve between halfway points using actual points as control points
+      ctx.quadraticCurveTo(point.x, point.y, control.x, control.y);
+    }
+
+    ctx.lineTo(cv.width, channel[channel.length - 1].y);
+    ctx.lineTo(cv.width, cv.height / 2);
+    ctx.fill();
+  })
+
+  let now = Date.now();
+  if (lastFrameTime) {
+    frameTimes.push(now - lastFrameTime);
+  }
+  if (frameTimes.length >= 120) {
+    frameTimes = frameTimes.slice(1);
+    const fps = 1000 / frameTimes.reduce((a, v) => a + v) * frameTimes.length;
+    const text = `FPS: ${fps.toFixed(1)}`;
+    const textWidth = ctx.measureText(text).width;
+    const topOffset = audioElt.clientHeight + 10 + 10; // 10 for padding, 10 for text height
+    ctx.fillText(text, ctx.canvas.width - textWidth - 10, topOffset)
+  }
+  lastFrameTime = now;
+  window.requestAnimationFrame(playAnim);
+}
+
+function roundTo(number, precision) {
+  return Math.round(number / precision) * precision;
+}
+
+window.fillCache = () => {
+  for (let i = 0; i < audioElt.duration; i += .001) {
+    getFrame(i);
+  }
 }
